b540a56ec3777b88a9c6ea715d8878065c047e30
S.J. Blair <49010502+Steven-J-Blair@users.noreply.github.com>
2024-09-23 18:00:05 -0400

    link don't work, switched back to mp3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
  // Set your audio file URL here (can be MP3 or MP4)
- const AUDIO_URL = 'https://youtu.be/tPDtiMKL0sQ?si=j1oNWJvm__lv9waD';
+ const AUDIO_URL = 'WP Samples.mp3/Dragon Lore-WP Sample.mp3';
 
  const audioElement = document.getElementById('audio-element');
  const playPauseButton = document.getElementById('play-pause');
